40a140f64861c757c06f2ec8eef7f3f672af4583
David <yeghshd@gmail.com>
2016-10-01 01:25:11 -0400

    Add site properties persistence.

--- javascript/helpers.js
+++ javascript/helpers.js
@@ -1,5 +1,5 @@
 const EVENT_LOG = 1, LOGIC_LOG = 2, TEMP_LOG = 3, STORE_LOG = 4;
-const logEvents = false, logLogic = false, logTemp = true, logStore = true;
+const logEvents = false, logLogic = true, logTemp = true, logStore = true;
 
 function cleanURL(url) {
     const length = url.length;
@@ -43,26 +43,34 @@ function log(text, type) {
 function logMemoryUsed(item) {
     item = item || null;
     storage.getBytesInUse(item,function (bytes) {
-        log("Bytes used:" + bytes + " - " + ((100 * bytes)/storage.QUOTA_BYTES).toFixed(2) + "%", STORE_LOG);
+        log("Bytes used:" + bytes + " - " +
+            ((100 * bytes)/storage.QUOTA_BYTES).toFixed(2) + "%", STORE_LOG);
     });
 }
 function persistInstallTime() {
-    storage.set({'value': new Date()}, function() {
+    storage.set({'install_time': new Date()}, function() {
         log("time stored", STORE_LOG);
     });
 }
 
-function retrieveInstallTime() {
-    storage.get('value', function(value) {
-        log("time installed: " + String(new Date(value)), STORE_LOG);
+function retrieveInstallTime(callback) {
+    storage.get('install_time', function(result) {
+        log("time installed: " + String(Date(result)), STORE_LOG);
+        callback(new Date(result['install_time']));
     });
 }
-logMemoryUsed();
 
-persistInstallTime();
-logMemoryUsed();
-retrieveInstallTime();
+function persistSite(url, properties) {
+    var item = {};
+    item[url] = properties;
+    storage.set(item, function() {
+        log("site stored:" + url, STORE_LOG);
+    });
+}
 
-logMemoryUsed();
-
-retrieveInstallTime();
+function retrieveSite(url, callback) {
+    storage.get(url, function(result) {
+        log("site url:" + url + " - time limit:" + result[url].limit, STORE_LOG);
+        callback(new SiteProperties(result[url]));
+    });
+}
