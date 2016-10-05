8e55e65fe8afed3b42b5439e9a47408083f51419
David <yeghshd@gmail.com>
2016-10-04 23:35:15 -0400

    Create alarm and persistence logic

--- javascript/background.js
+++ javascript/background.js
@@ -1,7 +1,7 @@
-var state, curURL, startTime, stopTime, alarmTime;
+var state, curURL, startTime, stopTime, timeTillAlarm, timer, curProperties;
 var storage = chrome.storage.local;
 
-const ACTIVE = 0, CLOSED = 1;
+const ACTIVE = 0, CLOSED = 1, MIN_ACTIVE_TIME = 2;
 
 
 //SiteProperties Definition
@@ -38,18 +38,34 @@ function afterGetURL(newURL) {
 }
 
 function onUpdateEvent(newURL) {
+    clearTimeout(timer);
     persistData();
     newSite(newURL);
 }
 
 function onTerminationEvent() {
+    clearTimeout(timer);
     persistData();
     state = CLOSED;
+    curURL = null;
+    startTime = null;
+    stopTime = null;
+    timeTillAlarm = null;
+    curProperties = null;
 }
 
 function persistData() {
-    log("persisting data", LOGIC_LOG);
     stopTime = new Date().getTime();
+    var activeTime = Math.round((stopTime - startTime) / 1000);
+
+    if (activeTime > MIN_ACTIVE_TIME && startTime > 0) {
+        log("persisting data: " + curURL + " :+" + activeTime, LOGIC_LOG);
+        persistSite(curURL, new SiteProperties(curProperties.getLimit(),
+            curProperties.getUsedSoFar() + activeTime, curProperties.getLimitEnabled()));
+    }
+    else {
+        log("only spent " + activeTime + " sec on site", LOGIC_LOG);
+    }
 }
 
 function newSite(newURL) {
@@ -57,27 +73,27 @@ function newSite(newURL) {
     log("new site", LOGIC_LOG);
     state = ACTIVE;
     startTime= new Date().getTime();
-    //retrieveSite(curURL,updateAlarmTime);
+    retrieveSite(curURL,updateAlarmTime);
 }
 
 function updateAlarmTime(properties) {
-    alarmTime = null;
-    if (properties.getLimitEnabled()) {
-        log(properties.getLimit(), LOGIC_LOG);
+    curProperties = properties;
+    timeTillAlarm = null;
+    if (curProperties.getLimitEnabled()) {
+        timeTillAlarm = curProperties.getLimit() - curProperties.getUsedSoFar();
+        timer = setTimeout(alarm, timeTillAlarm * 1000);
+        log("limit is: " + curProperties.getLimit() +
+            " ,timer in " + timeTillAlarm, LOGIC_LOG);
     }
 }
 
+function alarm() {
+    log("!ALARM!", LOGIC_LOG);
+}
 
 
 function onPopup () {
 }
 
 function onMainBtnClick() {
-    
 }
-
-
-
-
-
-
