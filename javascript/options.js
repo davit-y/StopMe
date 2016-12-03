c5db38abe326ac066f27203b6c98c96ba163c9a3
David <yeghshd@gmail.com>
2016-12-03 18:16:03 -0500

    Update options page

--- javascript/options.js
+++ javascript/options.js
@@ -4,9 +4,7 @@ function populate_options(allData) {
     var keys = Object.keys(allData);
     var tbl = document.getElementById("data_table");
     for (var i = 0; i < keys.length; ++i) {
-        if (keys[i].charAt(0) == '$') {
-            addRow(tbl, keys[i], allData[keys[i]]);
-        }
+        addRow(tbl, keys[i], allData[keys[i]]);
     }
 }
 
@@ -14,10 +12,10 @@ function addRow(tbl, key, value) {
     tbl.innerHTML = tbl.innerHTML +
         "<tr>" +
         "<td>" + key.substr(1) + "</td>" +
-        "<td>" + value.usedSoFar + "</td>" +
+        "<td>" + bgPage.secondsToText(value.usedSoFar) + "</td>" +
         "<td>" + value.limit + "</td>" +
         "<td>" + value.limitEnabled + "</td>" +
         "</tr>";
 }
 
-document.addEventListener('DOMContentLoaded', bgPage.retrieveAll(populate_options));
+document.addEventListener('DOMContentLoaded', bgPage.retrieveAllSites(populate_options));
